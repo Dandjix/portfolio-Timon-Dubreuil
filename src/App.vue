71f5aac307f88e23643b1561001cecd7c160b5c4
Timon Dubreuil <timondubreuil@outlook.com>
2025-06-12 15:44:57 +0200

    mode darksasuke ok

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,16 @@
 </template>
 
 <script setup>
-//
+import { Dark } from 'quasar'
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  Dark.set(true)
+} else {
+  Dark.set(false)
+}
+
+// Optional: Listen for changes in preference
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  Dark.set(e.matches)
+})
 </script>
